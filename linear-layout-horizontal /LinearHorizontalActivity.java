55cfb333992c2e7de5be2b55413f67bbebf351a2
Sanket Gajanan Narute <sanketnarute3007@gmail.com>
2026-04-13 06:33:44 +0530

    Create LinearHorizontalActivity.java

---
+++ linear-layout-horizontal /LinearHorizontalActivity.java
@@ -0,0 +1,14 @@
+package com.example.appdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearHorizontalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_horizontal);
+    }
+}

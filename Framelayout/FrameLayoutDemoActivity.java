0bf83e7416394f58bd2e9c3cb26e7d1740cc0a91
Sanket Gajanan Narute <sanketnarute3007@gmail.com>
2026-04-13 06:19:32 +0530

    Create FrameLayoutDemoActivity.java

---
+++ Framelayout/FrameLayoutDemoActivity.java
@@ -0,0 +1,14 @@
+package com.example.appdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FrameLayoutDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_demo);
+    }
+}
